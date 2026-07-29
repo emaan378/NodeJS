6762564404eecadacb74e36e3e22f980d66b4b5a
Emaan Fatima <alizbat14@gmail.com>
2026-07-30 00:24:59 +0500

    Add math module with add function and subtract function

--- 01/hello.js
+++ 01/hello.js
@@ -1,1 +1,9 @@
-console.log("Hey there! I am JS");
+// One way
+// const math=require("./math")
+// console.log(math.add(2,4));
+// console.log(math.sub(2,4));
+
+// Second way
+const {add ,sub}=require("./math");
+console.log(add(4,2));
+console.log(sub(5,2))
